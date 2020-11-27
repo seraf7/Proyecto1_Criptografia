23842bd967bdb93dd8d218812717bdc830b32887
SandraRacilla <s.garcia.racilla@gmail.com>
2020-11-27 14:53:05 -0600

    Archivo Principal

---
+++ main.py
@@ -0,0 +1,20 @@
+#Criptografía 2021-1
+#Primer Proyecto Parcial
+#Castillo López Humberto Serafín
+#García Racilla Sandra
+#Sánchez Escobar Fernando
+#Ejecuta todos los algoritmos
+
+import sys
+import os 
+
+####Instalación de Bibliotecas necesarias para el proyecto
+#si no cuenta con alguno, descomente las líneas para su ejecución
+#os.system('pip install pandas')
+#os.system('pip install cryptography')
+#os.system(pip install pycrypto
+
+os.system('python cifrado_descifrado.py')
+os.system('python hashing.py')
+os.system('python sign_verify.py')
+
